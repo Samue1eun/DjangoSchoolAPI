4b344860be6c53fef51ad9a3ce49b51b55a225e4
samue1eun <samuel.eun.park@gmail.com>
2024-11-21 13:52:42 -0600

    added in the grade model

--- grade_app/models.py
+++ grade_app/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 from django.core import validators
+from student_app.models import Student
+from subject_app.models import Subject
 
 # Grade Model
 class Grade(models.Model):
     grade = models.DecimalField(unique = False, null=False, default = 100, max_digits=5, decimal_places=2)
+    a_subject = models.ForeignKey(Subject, on_delete=models.CASCADE, null=True, blank=True)
+    student = models.ForeignKey(Student, on_delete=models.CASCADE, null=True, blank=True)
